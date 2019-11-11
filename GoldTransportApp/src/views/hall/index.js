190fd8807fc0546c4ee71b4f4620c0f89f46619f
lunadis <luislopes116@outlook.com>
2019-11-10 22:48:01 -0300

    #routes config done -#Init hall screen

--- GoldTransportApp/src/views/hall/index.js
+++ GoldTransportApp/src/views/hall/index.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react';
-import {Text, View, Button} from 'react-native';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import styles from './styles';
 
 export class Hall extends Component {
   render() {
     return (
-      <View>
-        <Text> Hall </Text>
-        <Button
-          title="Navegar para auth"
-          onPress={() => this.props.navigation.navigate('auth')}
-        />
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.logoField}>
+          <Image
+            style={styles.logo}
+            resizeMode={'contain'}
+            source={require('../../../Assets/Icons/logo.png')}
+          />
+        </View>
+        <View style={styles.formContainer}>
+          <TouchableOpacity style={styles.button} title="Entrar">
+            <Text>Entar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} title="Entrar">
+            <Text>Cadastrar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.flagPraime}>
+          <Image
+            style={styles.logo}
+            resizeMode={'contain'}
+            source={require('../../../Assets/Icons/faixa.png')}
+          />
+        </View>
+        <StatusBar translucent={true} backgroundColor={'transparent'} />
+      </SafeAreaView>
     );
   }
 }
--- GoldTransportApp/src/views/home/index.js
+++ GoldTransportApp/src/views/home/index.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react';;
 import {Text, View} from 'react-native';
+
 
 export class Home extends Component {
   render() {
